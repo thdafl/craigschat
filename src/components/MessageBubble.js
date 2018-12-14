a9ce3f0c6c6876c3612bf4050eebbda87c87ee1b
inizio <newman.kcchow@gmail.com>
2018-12-14 14:45:37 +0800

    refactor(bubble): adjust message bubble layout

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -3,12 +3,14 @@ import Avatar from '@material-ui/core/Avatar';
 
 const MessageBubble = ({message}) => (
   <div style={{fontSize: '20px', display: 'flex', alignItems: 'center', margin: '10px'}}>
-    <div style={{display: 'flex', marginRight: '10px', width: '15%'}}>
-      <Avatar style={{width: '30px', height: '30px', marginRight: '10px'}} alt="user-avator" src={message.user.photpUrl} />
-      <div style={{display: 'flex', alignItems: 'center', color: 'gray', fontSize: '10px'}}>@{message.user.name}</div>
+    <Avatar style={{width: '30px', height: '30px', marginRight: '10px'}} alt="user-avator" src={message.user.photoUrl} />
+    <div>
+      <div style={{display: 'flex'}}>
+        <div style={{color: 'gray', fontSize: '10px'}}>@{message.user.name}</div>
+        <div style={{color: 'gray', fontSize: '8px', marginLeft: 10}}>{message.timestamp}</div>
+      </div>
+      <div style={{textAlign: 'left', paddingRight: '10px', width: '75%', fontSize: '20px'}}>{message.text}</div>
     </div>
-    <div style={{textAlign: 'left', paddingRight: '10px', width: '75%', fontSize: '20px'}}>{message.text}</div>
-    <div style={{color: 'gray', fontSize: '5px', width: '10%'}}>{message.timestamp}</div>
   </div>
 )
 
