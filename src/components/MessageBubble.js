b7da1458dd3507d8d70963f61aa595bcc7717248
inizio <newman.kcchow@gmail.com>
2018-12-14 15:10:15 +0800

    feat(timestamp): use timeagojs for message timestamp

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
+import moment from 'moment'
+import TimeAgo from 'react-timeago'
 
 const MessageBubble = ({message}) => (
-  <div style={{fontSize: '20px', display: 'flex', alignItems: 'center', margin: '10px'}}>
+  <div style={{fontSize: '20px', display: 'flex', margin: '10px'}}>
     <Avatar style={{width: '30px', height: '30px', marginRight: '10px'}} alt="user-avator" src={message.user.photoUrl} />
     <div>
-      <div style={{display: 'flex'}}>
+      <div style={{display: 'flex', marginBottom: 5}}>
         <div style={{color: 'gray', fontSize: '10px'}}>@{message.user.name}</div>
-        <div style={{color: 'gray', fontSize: '8px', marginLeft: 10}}>{message.timestamp}</div>
+        <div style={{color: 'gray', fontSize: '10px', marginLeft: 10}}><TimeAgo date={moment(message.timestamp).toDate()} minPeriod={30}/></div>
       </div>
-      <div style={{textAlign: 'left', paddingRight: '10px', width: '75%', fontSize: '20px'}}>{message.text}</div>
+      <div style={{textAlign: 'left', paddingRight: '10px', width: '75%', fontSize: '20px', wordBreak: 'break-all'}}>{message.text}</div>
     </div>
   </div>
 )
