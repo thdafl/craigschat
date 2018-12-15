b442999ff684eaf1f4a4b8aa78bd7afee1f7b16d
YCHino <a2106349@gmail.com>
2018-12-15 11:25:53 +0900

    Improve MessageBubble style

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -11,7 +11,7 @@ const MessageBubble = ({message}) => (
         <div style={{color: 'gray', fontSize: '10px'}}>@{message.user.name}</div>
         <div style={{color: 'gray', fontSize: '10px', marginLeft: 10}}><TimeAgo date={moment(message.timestamp).toDate()} minPeriod={30}/></div>
       </div>
-      <div style={{textAlign: 'left', paddingRight: '10px', width: '75%', fontSize: '20px', wordBreak: 'break-all'}}>{message.text}</div>
+      <div style={{textAlign: 'left', fontSize: '1.2rem', fontWeight: 300, wordBreak: 'break-all'}}>{message.text}</div>
     </div>
   </div>
 )
