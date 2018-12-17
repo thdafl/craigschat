b296c78569c049850369869ecb37756506575bb7
inizio <newman.kcchow@gmail.com>
2018-12-17 10:06:05 +0800

    feat(profile): adds ProfileCache for user profile fetching

---
+++ src/hocs/ProfileCache.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+import {firebaseDb} from '../config/firebase'
+
+const cache = {}
+const onLoad = {}
+
+class ProfileCache extends Component {
+  state = {
+    profile: null
+  }
+
+  componentDidMount() {
+    const {id} = this.props
+    
+    if (!cache[id]) {
+      // If not already fetching
+      if (!onLoad[id]) {
+        onLoad[id] = []
+        
+        firebaseDb.ref('users/' + id).on('value', snapshot => {
+          cache[id] = snapshot.val()
+
+          onLoad[id].forEach(cb => {
+            cb(cache[id])
+          })
+        })
+      }
+
+      onLoad[id].push(profile => this.setState({profile}))
+    } else {
+      this.setState({profile: cache[id]})
+    }
+  }
+  
+  render() {
+    return (
+      this.state.profile
+        ? this.props.cb(this.state.profile)
+        : null
+    )
+  }
+}
+
+
+
+export default function getProfile(id, cb) {
+  return <ProfileCache id={id} cb={cb}/>
+}
