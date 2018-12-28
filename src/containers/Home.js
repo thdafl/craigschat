de6c009fd2cf316b93d4f2d98973b637328ce5b7
YCHino <a2106349@gmail.com>
2018-12-28 16:55:16 +0900

    Just adding html to remove explanatory text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,8 @@ import '../App.css';
 import { firebaseDb } from '../config/firebase.js';
 import { withRouter } from 'react-router-dom';
 import ListCard from '../components/ListCard';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 
@@ -15,7 +17,8 @@ class Home extends Component {
       user: "",
       id: "",
       description : "",
-      chatRooms: []
+      chatRooms: [],
+      usersChatRooms: []
     }
 
     this.onGoToChatButtonClick = this.onGoToChatButtonClick.bind(this);
@@ -25,6 +28,7 @@ class Home extends Component {
     firebaseDb.ref('chatrooms').on('child_added', (snapshot) => {
       const ctr = snapshot.val()
       const chatrooms = this.state.chatRooms
+      // const usersChatRooms = this.state.usersChatRooms
 
       chatrooms.push({
         id: ctr.id,
@@ -36,6 +40,10 @@ class Home extends Component {
         roommembers: ctr.roommembers,
         archived: ctr.archived
       })
+
+      // if (this.props.user.loginUser.id === ctr.owner.id) {
+        console.log("&&&&", this.props.user)
+      
 
       this.setState({
         chatRooms : chatrooms
@@ -54,10 +62,24 @@ class Home extends Component {
           <Grid container>
             <Hidden mdDown>
               <Grid item xs={12} sm={12} md={4} lg={3} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <div style={{width: '90%', marginTop: '10px', position: 'sticky', top: '50px'}}>
-                  This section should be sticky and filters comes here to filter chatrooms by place/genre etc.
-                  and anything else, including ads maybe....
+                <div style={{width: '95%', marginTop: '10px', position: 'sticky', top: '50px'}}>
+                  <Paper style={{padding: 18}}>
+                    <div style={{display: 'flex', alignItems: 'flex-end', padding: 3}}>
+                      <Typography style={{fontSize: 20, marginRight: 10}}>5</Typography>
+                      <Typography style={{fontSize: 12, fontWeight: 100, color: 'gray', paddingBottom: 3}}>Chatrooms you own</Typography>
+                    </div>
+                    <div style={{display: 'flex', alignItems: 'flex-end', padding: 3}}>
+                      <Typography style={{fontSize: 20, marginRight: 10}}>7</Typography>
+                      <Typography style={{fontSize: 12, fontWeight: 100, color: 'gray', paddingBottom: 3}}>Chatrooms you joined</Typography>
+                    </div>
+                  </Paper>
+                  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginTop: '15px'}}>
+                    <Link to="/" style={{fontSize: 15, fontWeight: 100, color: 'gray', textDecoration: 'none', padding: 3}}>About</Link>
+                    <Link to="/" style={{fontSize: 15, fontWeight: 100, color: 'gray', textDecoration: 'none', padding: 3}}>Terms and Conditions</Link>
+                    <Link to="/" style={{fontSize: 15, fontWeight: 100, color: 'gray', textDecoration: 'none', padding: 3}}>Privacy Policy</Link>
+                  </div>
                 </div>
+                
               </Grid>
             </Hidden>
 
