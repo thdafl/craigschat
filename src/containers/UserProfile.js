ee9142fb7a1ecb56660017da62609b52c1311315
inizio <newman.kcchow@gmail.com>
2018-12-30 13:01:42 +0800

    feat(profile): update text attributes of user profile

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import { Card, Avatar, Button } from '@material-ui/core';
+import { Card, Avatar, Button, TextField } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 
 import {firebaseDb, firebaseAuth} from '../config/firebase'
 import { userLogout } from '../store/users/actions';
 
-class UserProfile extends Component {  
+class UserProfile extends Component {
+  state = {
+    isEditting: false,
+    profile: {}
+  }
+  
   deleteAccount = async () => {
     const {credential} = await firebaseAuth.signInWithPopup(require('../config/firebase')[this.props.user.loginUser.provider.split('.')[0] + 'Provider'])
     
@@ -24,9 +29,27 @@ class UserProfile extends Component {
     await this.props.logout()
     this.props.history.push("/")
   }
+
+  toggleEdit = () => {
+    this.setState({
+      isEditting: !this.state.isEditting,
+      profile: this.props.user.loginUser
+    })
+  }
+
+  onFieldChange = (e) => {
+    this.setState({profile: {...this.state.profile, [e.target.name]: e.target.value}})
+  }
+
+  onFormSubmit= e => {
+    e.preventDefault()
+
+    firebaseDb.ref('users/' + this.props.user.loginUser.id).set(this.state.profile, this.toggleEdit)
+  }
   
   render() {
     const {loginUser} = this.props.user
+    const {isEditting, profile} = this.state
 
     return loginUser ? (
       <div className="App" style={{height: '100vh'}}>
@@ -34,9 +57,21 @@ class UserProfile extends Component {
           <Card style={{display: 'flex', padding: 20, flexDirection: 'row', margin: 'auto', maxWidth: 700}}>
             <Avatar style={{width: '75px', height: '75px', marginRight: 40, top: 0}} src={loginUser.photoUrl} />
             <div style={{textAlign: 'left'}}>
-              <h2 style={{margin: 5, marginLeft: 0}}>{loginUser.name}</h2>
-              <div>{loginUser.email}</div>
+              {isEditting ? (
+                <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={this.onFormSubmit}>
+                  <TextField name="name" label="Name" value={profile.name} onChange={this.onFieldChange}/>
+                  <TextField name="email" label="Email" value={profile.email} type="email" onChange={this.onFieldChange}/>
+                  <TextField name="description" label="About me" value={profile.description} rows={4} onChange={this.onFieldChange} multiline/>
+                  <Button color="primary" variant="raised" type="submit">Save</Button>
+                </form>
+              ) : (
+                <>
+                  <h2 style={{margin: 5, marginLeft: 0}}>{loginUser.name}</h2>
+                  <div>{loginUser.email}</div>
+                </>
+              )}
             </div>
+            {!isEditting && <Button style={{marginLeft: 'auto'}} onClick={this.toggleEdit}>Edit</Button>}
           </Card>
 
           <Button style={{color: 'red'}} onClick={this.deleteAccount}>Delete Account</Button>
