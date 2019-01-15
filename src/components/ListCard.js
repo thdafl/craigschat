9bdf5fbfb0bc5f3d9987a220cecf5236bd4e72a0
YCHino <a2106349@gmail.com>
2019-01-16 00:03:49 +0900

    Additional UI update

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -47,7 +47,7 @@ const ListCard = ({onClick, owner, title, place, description, roommembers, image
     <Card className={cardContainer}>
       <CardActionArea onClick={onClick} style={{height: '100%'}}>
           <CardMedia
-            style={{height: '70%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}
+            style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}
             image={(image) ? image : "https://static.vecteezy.com/system/resources/previews/000/200/370/large_2x/simple-low-poly-background-vector.jpg"}
             title="Contemplative Reptile"
           >
@@ -69,15 +69,16 @@ const ListCard = ({onClick, owner, title, place, description, roommembers, image
               <div style={{display: 'flex'}}>{renderAvatars()}</div>
             </div>
 
-            <div style={{display: 'flex', textAlign: 'start', height: 20}}>
-              <div style={{display: 'flex', paddingRight: 5}}>
+            <div style={{display: 'flex', textAlign: 'start', height: 60, overflow: 'hidden', textOverflow: 'ellipsis'}}>
+              <Typography style={{fontSize: 13, fontWeight: 600, color: 'rgb(72, 72, 72)', paddingLeft: 3}}>{description}</Typography>
+              {/* <div style={{display: 'flex', paddingRight: 5}}>
                 <span role="img" aria-label="sheep">🔮</span>
                 <Typography style={{fontSize: 13, fontWeight: 600, color: 'black', paddingLeft: 3}}>2 Online</Typography>
               </div>
               <div style={{display: 'flex'}}>
                 <span role="img" aria-label="sheep">💡</span>
                 <Typography style={{fontSize: 13, fontWeight: 600, color: 'black', paddingLeft: 3}}>11 days ago</Typography>
-              </div>
+              </div> */}
             </div>
         </CardContent>
       </CardActionArea>
@@ -89,13 +90,13 @@ const styles = theme => ({
   cardContainer: {
     width: '100%',
     margin: 10,
-    // borderRadius: 0,
     boxShadow: 'none'
   },
   cardContent: {
     padding: 10,
     paddingLeft: 15,
     paddingRight: 15,
+    height: '90px'
   },
   ownerInfoWrapper: {
     display: 'flex',
